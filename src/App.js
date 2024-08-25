80dc595079eb08980214c9f13f2d8080ef2ae413
Arttu <arttu.sulkonen@icloud.com>
2024-08-25 10:39:10 +0300

    Initial commit with React and SCSS setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Flight Search App</h1>
+    </div>
+  );
+}
+
+export default App;
